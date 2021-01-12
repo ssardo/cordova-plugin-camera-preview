e3696932e20ba62adfbef0eb3b40fe8273e3f5ab
ssardo <sergio.sardo@gmail.com>
2021-01-12 15:37:54 +0000

    Update edit_manifest.js

--- hooks/android/edit_manifest.js
+++ hooks/android/edit_manifest.js
@@ -1,113 +1,105 @@
 // Global vars
 var deferral, fs, elementtree, path;
 var utils = require('./utilities');
-var cordovaAbove8 = utils.isCordovaAbove(this, 8);
-if (cordovaAbove8){
-  var fs = require("fs");
-  var path = require("path");
-  var elementtree = require("elementtree");
-} else {
-  var fs = requireCordovaModule("fs");
-  var path = requireCordovaModule("path"); 
-  var elementtree = requireCordovaModule("elementtree");
-}
-
-
-var disableAllowBackup = (function () {
-
-    var disableAllowBackup = {};
-
-    var manifestPaths = {
-        cordovaAndroid6: "platforms/android/AndroidManifest.xml",
-        cordovaAndroid7: "platforms/android/app/src/main/AndroidManifest.xml"
-    };
-
-    var rootDir;
-
-    disableAllowBackup.fileExists = function (filePath) {
-        try {
-            return fs.statSync(filePath).isFile();
-        } catch (error) {
-            return false;
-        }
-    };
-
-    disableAllowBackup.parseElementtreeSync = function (filename) {
-        var content = fs.readFileSync(filename, 'utf-8');
-        return new elementtree.ElementTree(elementtree.XML(content));
-    };
-
-    disableAllowBackup.getAndroidManifestPath = function () {
-        var cordovaAndroid6Path = path.join(rootDir, manifestPaths.cordovaAndroid6);
-        var cordovaAndroid7Path = path.join(rootDir, manifestPaths.cordovaAndroid7);
-
-        if (this.fileExists(cordovaAndroid6Path)) {
-            return cordovaAndroid6Path;
-        } else if (this.fileExists(cordovaAndroid7Path)) {
-            return cordovaAndroid7Path;
-        } else {
-            return undefined;
-        }
-    };
-
-
-    disableAllowBackup.apply = function (ctx) {
-        debugger;
-        rootDir = ctx.opts.projectRoot;
-
-        var androidManifestPath = this.getAndroidManifestPath();
-        if(!androidManifestPath) {
-            throw new Error("Unable to find AndroidManifest.xml");
-        }
-        
-        var manifestTree = this.parseElementtreeSync(androidManifestPath);
-        var root = manifestTree.getroot();
-
-        if (root) {
-			root.set("xmlns:tools", "http://schemas.android.com/tools");
-			
-			root._children.forEach(function(item) {
-				if(item.tag === 'uses-feature') {
-					item.set('android:required','true');
-					item.set('tools:node', 'merge');
-				}
-				
-				if(item.tag === 'uses-permission') {
-					item.set('tools:node', 'merge');
-				}
-			});
-				
-            fs.writeFileSync(androidManifestPath, manifestTree.write({indent:4}, 'utf-8'));
-        } else {
-            throw new Error("Invalid AndroidManifest.xml structure. No <manifest> tag found.");
-        }
-    };
-
-    return disableAllowBackup;
-})();
 
 module.exports = function (ctx) {
   var cordovaAbove8 = utils.isCordovaAbove(ctx, 8);
-  
-  if (cordovaAbove8) {
+
+  if (cordovaAbove8){
     var Q = require("q");
+    var fs = require("fs");
+    var path = require("path");
+    var elementtree = require("elementtree");
   } else {
-    var Q = requireCordovaModule("q");
+    var Q = requireCordovaModule("q");  
+    var fs = requireCordovaModule("fs");
+    var path = requireCordovaModule("path"); 
+    var elementtree = requireCordovaModule("elementtree");
   }
-  
-  var fs = require("fs");
-  var path = require("path");
-  var elementtree = require("elementtree");
-  
-    var deferral = Q.defer();
 
-    try {
-        disableAllowBackup.apply(ctx);
-        deferral.resolve();
-    } catch (error) {
-        deferral.reject(error);
-        return deferral.promise;
-    }
+  var cordovaAbove8 = utils.isCordovaAbove(ctx, 8);
+  deferral = Q.defer();
 
-    return deferral.promise;
+  var disableAllowBackup = (function () {
+
+      var disableAllowBackup = {};
+
+      var manifestPaths = {
+          cordovaAndroid6: "platforms/android/AndroidManifest.xml",
+          cordovaAndroid7: "platforms/android/app/src/main/AndroidManifest.xml"
+      };
+
+      var rootDir;
+
+      disableAllowBackup.fileExists = function (filePath) {
+          try {
+              return fs.statSync(filePath).isFile();
+          } catch (error) {
+              return false;
+          }
+      };
+
+      disableAllowBackup.parseElementtreeSync = function (filename) {
+          var content = fs.readFileSync(filename, 'utf-8');
+          return new elementtree.ElementTree(elementtree.XML(content));
+      };
+
+      disableAllowBackup.getAndroidManifestPath = function () {
+          var cordovaAndroid6Path = path.join(rootDir, manifestPaths.cordovaAndroid6);
+          var cordovaAndroid7Path = path.join(rootDir, manifestPaths.cordovaAndroid7);
+
+          if (this.fileExists(cordovaAndroid6Path)) {
+              return cordovaAndroid6Path;
+          } else if (this.fileExists(cordovaAndroid7Path)) {
+              return cordovaAndroid7Path;
+          } else {
+              return undefined;
+          }
+      };
+
+
+      disableAllowBackup.apply = function (ctx) {
+          debugger;
+          rootDir = ctx.opts.projectRoot;
+
+          var androidManifestPath = this.getAndroidManifestPath();
+          if(!androidManifestPath) {
+              throw new Error("Unable to find AndroidManifest.xml");
+          }
+
+          var manifestTree = this.parseElementtreeSync(androidManifestPath);
+          var root = manifestTree.getroot();
+
+          if (root) {
+        root.set("xmlns:tools", "http://schemas.android.com/tools");
+
+        root._children.forEach(function(item) {
+          if(item.tag === 'uses-feature') {
+            item.set('android:required','true');
+            item.set('tools:node', 'merge');
+          }
+
+          if(item.tag === 'uses-permission') {
+            item.set('tools:node', 'merge');
+          }
+        });
+
+              fs.writeFileSync(androidManifestPath, manifestTree.write({indent:4}, 'utf-8'));
+          } else {
+              throw new Error("Invalid AndroidManifest.xml structure. No <manifest> tag found.");
+          }
+      };
+
+      return disableAllowBackup;
+  })();
+
+      try {
+          disableAllowBackup.apply(ctx);
+          deferral.resolve();
+      } catch (error) {
+          deferral.reject(error);
+          return deferral.promise;
+      }
+
+      return deferral.promise;
 };
